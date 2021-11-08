bd09de726167d46dd34d29bf07032b740695d412
Maksim <maksimukoloff@yandex.ru>
2021-11-08 20:15:19 +0300

    Checkbox

--- public/script.js
+++ public/script.js
@@ -12,15 +12,67 @@ let tasksElem = document.querySelector('.tasks')
 // }
 
 let taskListHtml = ''
-taskList.forEach(/*callback функция*/function(task){/*на каждой итерации цикла forEach вызывается функция task*/
+taskList.forEach(/*callback функция*/function(task, index) {/*на каждой итерации цикла forEach вызывается функция task*/
     const taskHtml =`
-    <li class="task-card">
-        <div class="task-name">${task}</div>
+    <li class="task-card" name="task-card-${index}">
+   <div class = "task-card__info">
+      <div class = "task-card__input-block">
+            <input class ="check-input" type="checkbox" data-index = "${index}">
+            <div class = "task-card__status">In work</div>
+      </div>            
+      <div class="task-name" data-index="${index}">${task}</div>
+   </div>
+   <div>
         <button class="edit">Edit</button>
         <button class="delete">Delete</button>
-   </li>`
+   </div>
+        </li>`
    taskListHtml += taskHtml/*складываем строки*/
 })
 
-console.log(taskListHtml);
+// tasksElem.onclick = function(event) {/*функция при клике на tasksElem(любой элемент всё выводится в консоль) */
+//    console.log(event.target);
+// }
+
+tasksElem.addEventListener('click', function(event){ /*обработчик при клике на tasksElem(любой элемент всё выводится в консоль) тоже самое что и сверху,только если addEventListener будет несколько,то и в консоль будет выводиться несколко значений,а в  onclick нет только 1 раз*/
+  const checkInputElem = event.target.closest('.check-input[type=checkbox]')
+   if(checkInputElem){/*метод closest смотрит на ближашего родителя с указанным селектором проверяем что мы кликнули именно по чекбоксу*/
+         toggleTextDecor(checkInputElem)/*вызывается функция  отвечающая перечеркнуть текст или убрать перечёркивание */
+      return
+   }
+   if(window.matchMedia("(max-width: 400px)").matches)/*проверяем размер экрана*/
+      {/*то же самое что и верхняя функция только для телефонов*/
+      const taskNameElem = event.target.closest('.task-name')
+         if (taskNameElem){
+            toggleCheckbox(taskNameElem)/*тогда запускаем эту функциюкоторая находит чекбокс,то она перчеркивает текст и вкл и выкл чекбокс */
+         }
+         
+      }
+   
+}/*, {once: true}*/) /*чтобы обработчик сработал только 1 раз в онклике так не сделать */
+
+function toggleTextDecor(checkbox){
+   if(checkbox.checked){
+      checkbox.nextElementSibling.textContent = 'Done'
+      checkbox.parentElement.nextElementSibling.style.textDecoration = 'line-through'
+   }else{
+      checkbox.nextElementSibling.textContent = 'In work'
+      checkbox.parentElement.nextElementSibling.style.textDecoration = null
+   }
+}
+
+function toggleCheckbox(textElem){
+   const checkbox = textElem.previousElementSibling.firstElementChild
+   if (textElem.style.textDecoration){
+      textElem.style.textDecoration = null
+      checkbox.checked = false
+      checkbox.nextElementSibling.textContent = 'In work'
+
+   }  else{
+      textElem.style.textDecoration = 'line-through'
+      checkbox.checked = true
+      checkbox.nextElementSibling.textContent = 'Done'
+   }
+}
+
 tasksElem.innerHTML = taskListHtml
