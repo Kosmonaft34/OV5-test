e6c92b03e7481d9e87196521d6237e8ed6f226a7
Maksim <maksimukoloff@yandex.ru>
2021-11-15 20:04:00 +0300

    nodejs $ scriptts

--- public/script.ts
+++ public/script.ts
@@ -1,3 +1,14 @@
+// @ts-check
+
+/**
+ * Jquery analog
+ * @param {string} selector Slector
+ * @returns {Element} Element
+ */
+
+// function $(selector:String){
+//    return document.querySelector(selector)
+// }
 let taskList = ['Task 1:Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit voluptatum quidem animi minus molestiae possimus. Ad, obcaecati animi minima atque magni quaerat esse tenetur! Impedit odit illo veritatis optio. Illum?',
 ' Task 2:Beatae illo repellat earum ab facilis obcaecati consequatur at blanditiis dignissimos magnam nostrum aliquam ducimus dolores corporis autem, debitis, quis, voluptates aliquid porro! Quia vero recusandae rem praesentium fugit neque.',
 'Task 3:Eius provident cum voluptas vero iusto, quo quaerat odio, delectus corporis cupiditate nobis odit. Veritatis laborum nisi illum ullam quibusdam facilis facere, quisquam vitae. Hic quaerat suscipit eaque officiis consequuntur.',
@@ -34,22 +45,22 @@ taskList.forEach(/*callback функция*/function(task, index) {/*на каж
 //    console.log(event.target);
 // }
 
-tasksElem.addEventListener('click', function(event){ /*обработчик при клике на tasksElem(любой элемент всё выводится в консоль) тоже самое что и сверху,только если addEventListener будет несколько,то и в консоль будет выводиться несколко значений,а в  onclick нет только 1 раз*/
-  const checkInputElem = event.target.closest('.check-input[type=checkbox]')
-   if(checkInputElem){/*метод closest смотрит на ближашего родителя с указанным селектором проверяем что мы кликнули именно по чекбоксу*/
-         toggleTextDecor(checkInputElem)/*вызывается функция  отвечающая перечеркнуть текст или убрать перечёркивание */
-      return
-   }
-   if(window.matchMedia("(max-width: 400px)").matches)/*проверяем размер экрана*/
-      {/*то же самое что и верхняя функция только для телефонов*/
-      const taskNameElem = event.target.closest('.task-name')
-         if (taskNameElem){
-            toggleCheckbox(taskNameElem)/*тогда запускаем эту функциюкоторая находит чекбокс,то она перчеркивает текст и вкл и выкл чекбокс */
-         }
+// tasksElem.addEventListener('click', function(event){ /*обработчик при клике на tasksElem(любой элемент всё выводится в консоль) тоже самое что и сверху,только если addEventListener будет несколько,то и в консоль будет выводиться несколко значений,а в  onclick нет только 1 раз*/
+//   const checkInputElem = event.target.closest('.check-input[type=checkbox]')
+//    if(checkInputElem){/*метод closest смотрит на ближашего родителя с указанным селектором проверяем что мы кликнули именно по чекбоксу*/
+//          toggleTextDecor(checkInputElem)/*вызывается функция  отвечающая перечеркнуть текст или убрать перечёркивание */
+//       return
+//    }
+//    if(window.matchMedia("(max-width: 400px)").matches)/*проверяем размер экрана*/
+//       {/*то же самое что и верхняя функция только для телефонов*/
+//       const taskNameElem = event.target.closest('.task-name')
+//          if (taskNameElem){
+//             toggleCheckbox(taskNameElem)/*тогда запускаем эту функциюкоторая находит чекбокс,то она перчеркивает текст и вкл и выкл чекбокс */
+//          }
          
-      }
+//       }
    
-}/*, {once: true}*/) /*чтобы обработчик сработал только 1 раз в онклике так не сделать */
+// }/*, {once: true}*/) /*чтобы обработчик сработал только 1 раз в онклике так не сделать */
 
 function toggleTextDecor(checkbox){
    if(checkbox.checked){
